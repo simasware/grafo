e00c3b0df402a58f7171cfd77be628c35030eebe
Fernando <simasware@OSIRIS>
2013-06-20 22:43:10 -0300

    adicionada classe para implementação do problema do caixeiro viajante.

---
+++ src/grafo2/dominio/CaixeiroViajante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafo2.dominio;
+
+/**
+ * Implementação do Caixeiro viajante.
+ * @author simasware
+ */
+public class CaixeiroViajante {
+    private int[][] matriz;
+    private int tamanho;
+    private final String alfabeto = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    
+    public CaixeiroViajante(Grafo g){
+        this.tamanho = g.getVerticesCount();
+        this.matriz = new int[tamanho][tamanho];
+        for (Ligacao l : g.getLigacoes()) {
+            int idOrigem = alfabeto.indexOf(l.getOrigem().getId());
+            int idDestino = alfabeto.indexOf(l.getDestino().getId());            
+            this.matriz[idOrigem][idDestino] = l.getValue();
+        }
+    }
+}
